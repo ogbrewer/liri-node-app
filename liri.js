3d4058b5da6620abf043ca5b180bf1389ff27375
ogbrewer <47515020+ogbrewer@users.noreply.github.com>
2019-03-21 00:09:58 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 var keys = require("./key.js");
 var Spotify = require('node-spotify-api');
@@ -71,10 +70,57 @@ if (process.argv[2] === "do-what-it-says") {
         console.log(data);
 
         var dataArr = data.split(",");
-
+        process.argv[0]="node";
+        process.argv[1]="liri.js"
         process.argv[2]=dataArr[0];
         process.argv[3]=dataArr[1];
+        console.log(process.argv[3]);
 
+        if (process.argv[2] === "spotify-this-song") {
 
+            spotify.search({ type: 'track', query: process.argv[3] }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+        
+                console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 1));
+                console.log(JSON.stringify(data.tracks.items[0].name, null, 1));
+                console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 1));
+                console.log(JSON.stringify(data.tracks.items[0].album.name, null, 1));
+        
+            });
+        
+        }
+        if (process.argv[2] === "concert-this") {
+            var artist = process.argv[3];
+            var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+            axios.get(queryUrl).then(
+                function (response) {
+                    console.log(response.data[0].venue.name);
+                    console.log(response.data[0].venue.country);
+                    console.log(response.data[0].venue.region);
+                    console.log(response.data[0].venue.city);
+                    console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+                }
+            )
+        }
+        if (process.argv[2] === "movie-this") {
+            var movieName = process.argv[3];
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        
+            axios.get(queryUrl).then(
+                function (response) {
+                    console.log(response.data.Title);
+                    console.log(response.data.Year);
+                    console.log(response.data.imdbRating);
+                    console.log(response.data.Ratings[1].Value);
+                    console.log(response.data.Language);
+                    console.log(response.data.Plot);
+                    console.log(response.data.Country);
+                    console.log(response.data.Actors);
+        
+                }
+            );
+        }
     });
 }
