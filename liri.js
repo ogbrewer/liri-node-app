68dd457d07062868284449532c76192da81075a1
Oliver Gordon Brewer IV <olivergordonbreweriv@Olivers-MacBook-Air.local>
2019-03-20 22:14:22 -0700

    push comes to shove

--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
+
 require("dotenv").config();
 var keys = require("./key.js");
 var Spotify = require('node-spotify-api');
-var axios = require("axios")
+var axios = require("axios");
+var moment = require("moment");
+
+
 //console.log(keys);
 
 var spotify = new Spotify(keys.spotify);
@@ -9,13 +13,16 @@ var spotify = new Spotify(keys.spotify);
 
 if (process.argv[2] === "spotify-this-song") {
 
-
     spotify.search({ type: 'track', query: process.argv[3] }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-        console.log(JSON.stringify(data, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 1));
+        console.log(JSON.stringify(data.tracks.items[0].name, null, 1));
+        console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 1));
+        console.log(JSON.stringify(data.tracks.items[0].album.name, null, 1));
+
     });
 
 }
@@ -41,13 +48,14 @@ if (process.argv[2] === "movie-this") {
 
 if (process.argv[2] === "concert-this") {
     var artist = process.argv[3];
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryUrl).then( 
     function(response) {
-        console.log(response.data);
-       
-
+        console.log(response.data[0].venue.name);
+        console.log(response.data[0].venue.country);
+        console.log(response.data[0].venue.region);
+        console.log(response.data[0].venue.city);
+        console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
       }
-    );
+    )
 }
