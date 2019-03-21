a8d339ec6129450c7acf80139eebcd59e6b1504e
Oliver Gordon Brewer IV <olivergordonbreweriv@Olivers-MacBook-Air.local>
2019-03-20 23:20:11 -0700

    shit

--- liri.js
+++ liri.js
@@ -31,31 +31,50 @@ if (process.argv[2] === "movie-this") {
     var movieName = process.argv[3];
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(queryUrl).then( 
-    function(response) {
-        console.log(response.data.Title);
-        console.log(response.data.Year);
-        console.log(response.data.imdbRating);
-        console.log(response.data.Ratings[1].Value);
-        console.log(response.data.Language);
-        console.log(response.data.Plot);
-        console.log(response.data.Country);
-        console.log(response.data.Actors);
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data.Title);
+            console.log(response.data.Year);
+            console.log(response.data.imdbRating);
+            console.log(response.data.Ratings[1].Value);
+            console.log(response.data.Language);
+            console.log(response.data.Plot);
+            console.log(response.data.Country);
+            console.log(response.data.Actors);
 
-      }
+        }
     );
 }
 
 if (process.argv[2] === "concert-this") {
     var artist = process.argv[3];
-        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-    axios.get(queryUrl).then( 
-    function(response) {
-        console.log(response.data[0].venue.name);
-        console.log(response.data[0].venue.country);
-        console.log(response.data[0].venue.region);
-        console.log(response.data[0].venue.city);
-        console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
-      }
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data[0].venue.name);
+            console.log(response.data[0].venue.country);
+            console.log(response.data[0].venue.region);
+            console.log(response.data[0].venue.city);
+            console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        }
     )
+}
+
+if (process.argv[2] === "do-what-it-says") {
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        process.argv[2]=dataArr[0];
+        process.argv[3]=dataArr[1];
+
+
+    });
 }
